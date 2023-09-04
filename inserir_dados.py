5f9c48d7d4af649040e5c2d036bc7b3b1a4ea13d
Luiz-Dinani <luiz.martinsFO@bandtec.com.br>
2023-09-04 04:12:24 -0300

    Anonimizando endereço

--- inserir_dados.py
+++ inserir_dados.py
@@ -108,14 +108,43 @@ inserirDados(insert)
 insert = 'insert into funcionario (nome,cpf,email,idade,genero,senha,fkEmpresa,funcao) values \n'
 for dados in open('funcionario.csv', 'r', encoding='UTF-8'):
     info = dados.split(';')
-    insert+=f"('{info[0]}','{info[1].zfill(11)}','{info[4]}',{info[3]},'{info[2]}','{info[5]}',{int(info[7])},'{info[6]}'),\n"
+    insert += f"('{info[0]}','{info[1].zfill(11)}','{info[4]}',{info[3]},'{info[2]}','{info[5]}',{int(info[7])},'{info[6]}'),\n"
 insert = insert[:-2]
 insert += ';'
 inserirDados(insert)
 insert = 'insert into tanque (fkFuncionario,cep,cidade,bairro,rua,estado) values \n'
+
+
+def anonimizar_string(texto):
+    texto_separado = texto.split()
+
+    len_texto = len(texto_separado)
+    if len_texto <= 2:
+        len_metade_palavra = len(texto_separado[-1]) // 2
+        texto_separado[-1] = "*" * len_metade_palavra + texto_separado[-1][len_metade_palavra:]
+    else:
+        i = 0
+        for palavra in texto_separado:
+            if i != 0 and ((i != len_texto - 1 and len_texto != 2) or len(texto_separado[i-1]) <= 3):
+                texto_separado[i] = len(palavra) * "*"
+            i += 1
+
+    texto_anonimizado = " ".join(texto_separado)
+    return texto_anonimizado
+
+def anonimizar_endereco(cep, cidade, bairro, rua):
+    cep_anonimizado = cep[:2] + "*" * (len(cep)-2) + cep[len(cep)-1:]
+    cidade_anonimizado = anonimizar_string(cidade)
+    bairro_anonimizado = anonimizar_string(bairro)
+    rua_anonimizado = anonimizar_string(rua)
+    estado_anonimizado = 'XX'
+    return cep_anonimizado, cidade_anonimizado, bairro_anonimizado, rua_anonimizado, estado_anonimizado
+
 for dados in open('dados.csv', 'r', encoding='UTF-8'):
     info = dados.split(';')
-    insert+=f"({int(info[5])},'{info[0].zfill(8)}','{info[1]}','{info[2]}','{info[3]}','{info[4]}'),\n"
+    cep, cidade, bairro, rua, estado = info[0], info[1], info[2], info[3], info[4]
+    cep_anonimizado, cidade_anonimizado, bairro_anonimizado, rua_anonimizado, estado_anonimizado = anonimizar_endereco(cep, cidade, bairro, rua)
+    insert += f"({int(info[5])},'{cep_anonimizado.zfill(8)}','{cidade_anonimizado}','{bairro_anonimizado}','{rua_anonimizado}','{estado_anonimizado}'),\n"
 insert = insert[:-2]
 insert += ';'
 inserirDados(insert)
