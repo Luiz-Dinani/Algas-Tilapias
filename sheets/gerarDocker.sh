c826137dab4e3708c6b3a9302a6ba7bc7f880c4b
Luiz-Dinani <luiz.martinsFO@bandtec.com.br>
2023-09-17 02:21:49 -0300

    Voltando para a porta 5000

--- sheets/gerarDocker.sh
+++ sheets/gerarDocker.sh
@@ -3,7 +3,8 @@ python3 atualizarAWSCredentials.py
 docker stop samaka-api-ct
 docker rm samaka-api-ct
 
-docker rm image samaka-api-img
+docker image rm samaka-api-img
 docker image build -t samaka-api-img .
-docker container run --name samaka-api-ct -p 8080:8080 -d --restart unless-stopped samaka-api-img
+
+docker container run --name samaka-api-ct -p 5000:5000 -d --restart unless-stopped samaka-api-img
 docker start samaka-api-ct
