069af99d1ee861a2ec0865f8482ddf8e00c96469
JuanSilva2001 <juan.silva@bandtec.com.br>
2023-10-05 19:38:41 -0300

    Homologação de leitura de imagem

---
+++ desenvolvimento_leitura_arquivo/leitura_imagem/leitura_imagem.py
@@ -0,0 +1,8 @@
+# Importando o módulo Pillow (Manipulação de Imagens) para abrir a imagem no script
+from PIL import Image
+# Módulo para a utilização da tecnologia OCR
+import pytesseract
+# Extraindo o texto da imagem
+print('A')
+print( pytesseract.image_to_string( Image.open('teste-com-python-ocr.jpg'), lang='por') )
+print('F')
---
+++ instalador.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install tesseract-ocr
+sudo apt install python3-pip
+pip install pillow
+pip install pytesseract
